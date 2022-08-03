102f195253fa8e863d30ae3caa578ed6cf02b761
김민규 <gimmingyu@gimmingyuui-Macmini.local>
2022-08-03 11:40:51 +0900

    Add shadowing

---
+++ shadowing/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 4;
+    let x = x + 1;
+    let x = x * 2;
+    println!("x is {}", x);    
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("spaces is {}", spaces);
+}
